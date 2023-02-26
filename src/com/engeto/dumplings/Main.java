5822388126188ec9fdcd17feea8b92788f944298
Pavel Svejda <pavlis.1@seznam.cz>
2023-02-26 01:49:25 +0100

    Cleanup old code from Main.java and add new one
    - add racesDB
    - work with list of racers in the Race

--- src/com/engeto/dumplings/Main.java
+++ src/com/engeto/dumplings/Main.java
@@ -1,51 +1,63 @@
 package com.engeto.dumplings;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Patri k lekci 2.3 - vyklad teorie.
+ * Patri k lekci 2 - cviceni k vykladu teorie
  */
 public class Main {
 
-    // protoze jsme vytvorili konstruktor, tato metoda se stala zbytecnou
-//    private static Racer createRacer(
-//            String name, LocalDate born, int numberOfDumplins, boolean finalResult ) {
-//        Racer newRacer = new Racer();
-//        newRacer.setName(name);
-//        newRacer.setBorn(born);
-//        newRacer.setNumberOfDumplings(numberOfDumplins);
-//        newRacer.setFinalResult(finalResult);
-//
-//        return newRacer;
-//    }
+    private static void racersNames(Race race) {
+        for (Racer racer : race.getRacers()) {
+            System.out.println(racer);
+        }
+    }
 
     public static void main(String[] args) {
-        Racer firstRacer = new Racer();
-        Racer secondRacer = new Racer();
+//        Racer firstRacer = new Racer();
+//        Racer secondRacer = new Racer();
 
-        Racer race2017 = new Racer();
-        Racer race2018 = new Racer();
-        Racer race2019 = new Racer();
+        List<Race> racesDB = new ArrayList<>();
 
-        firstRacer.setName("Karel");
-        System.out.println(firstRacer.getName());
-        secondRacer.setName("Jana");
-        System.out.println(secondRacer.getName());
+        Race race2017 = new Race(2017);
+        Race race2018 = new Race(2018);
+        Race race2019 = new Race(2019);
 
-        Racer chuck = new Racer("Chuck Norris", LocalDate.of(1956, 8, 11), 100);
+        racesDB.add(race2017);
+        racesDB.add(race2018);
+        racesDB.add(race2019);
+        for (Race race : racesDB) {
+            System.out.println(race.getYear());
+        }
 
-        String chucksName = chuck.getName();
-        System.out.println(chuck.getNumberOfDumplings());
+        race2018.addRacer(new Racer("James", LocalDate.of(1975, 10, 4)));
+        race2018.addRacer(new Racer("John", LocalDate.of(1965, 3, 6)));
+        race2018.addRacer(new Racer("Jim", LocalDate.of(1966, 6, 5)));
+        race2018.addRacer(new Racer("John Jameson", LocalDate.of(1976, 5, 9)));
+        race2018.addRacer(new Racer("James Johnson", LocalDate.of(1976, 5, 8)));
 
-        Race todaysRace = new Race(2020, "MČR Břicháč Cup", LocalDate.of(2020, 10, 27));
-        // je hotovy konstruktor (radek vys), takze toto uz neni zapotrebi
-//        todaysRace.setYear(2020);
-//        todaysRace.setTitle("MČR Břicháč Cup");
-//        todaysRace.setDate(LocalDate.of(2020, 10, 27));
+        racersNames(race2018);
+        race2018.removeRacer(2);
+        System.out.println("--------");
+        racersNames(race2018);
 
-        // ...a nakonec stejne zvitezil nekdo jiny, nez Chuck.
-        Racer stranger = new Racer();
-        stranger.setName("Charles");
-        todaysRace.setWinner(stranger);
+//        firstRacer.setName("Karel");
+//        System.out.println(firstRacer.getName());
+//        secondRacer.setName("Jana");
+//        System.out.println(secondRacer.getName());
+//
+//        Racer chuck = new Racer("Chuck Norris", LocalDate.of(1956, 8, 11), 100);
+//
+//        String chucksName = chuck.getName();
+//        System.out.println(chuck.getNumberOfDumplings());
+
+//        Race todaysRace = new Race(2020, "MČR Břicháč Cup", LocalDate.of(2020, 10, 27));
+//
+//        // ...a nakonec stejne zvitezil nekdo jiny, nez Chuck.
+//        Racer stranger = new Racer();
+//        stranger.setName("Charles");
+//        todaysRace.setWinner(stranger);
     }
 }
